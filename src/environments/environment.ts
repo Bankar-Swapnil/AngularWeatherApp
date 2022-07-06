2c4ab59a28173e0f5a6778e984a3f1fcfebc6e99
Swapnil Bankar <98152507+Bankar-Swapnil@users.noreply.github.com>
2022-07-06 16:23:40 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,14 @@
 
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: '...',
+    authDomain: 'project.firebaseapp.com',
+    databaseURL: 'https://project.firebaseio.com',
+    projectId: 'project',
+    storageBucket: 'project.appspot.com',
+    messagingSenderId: '...',
+  },
   weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
   XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
   XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
