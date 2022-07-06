5fb794cfd9350c3b2a0b507721e2fc215eddc3db
Swapnil Bankar <swapnilbankar1010@gmail.com>
2022-07-06 16:03:20 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit {
+
+    constructor(private weatherService: WeatherService) {
+
+    }
+    ifTrue: boolean = false;
+    cityName: string = 'pune';
+    weatherData?: WeatherData;
+
+    ngOnInit(): void {
+        this.getWeatherData(this.cityName);
+        this.cityName = '';
+    }
+
+    onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+    }
+
+    private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName)
+    .subscribe({
+        next: (response) => {
+        this.weatherData = response;
+        if(this.weatherData?.main?.temp >19){
+            this.ifTrue=true;
+        }
+        else{
+            this.ifTrue=false;
+        }
+        console.log(response);
+        }
+    });
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'cd35a02fd0msh35e22914ac35b9ep123ef8jsnb4da4b1f715a'
 };
 
 /*
